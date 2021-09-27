763843a7cff5c6856ee47ed78d001058e2c08ab6
Sener <Sener@Sener-Lenovo-Pro>
2021-09-27 13:05:55 +0200

    bilder

---
+++ Scripts/Aufgabe_8ab.sql
@@ -0,0 +1,33 @@
+
+
+
+/* Aufgabe 8a select distinct einmalige Werte erstellen.  */
+/*
+select distinct
+	fsi.fahrweg_id, 
+    fsi.linie,
+    fsi.richtung,
+    fsi.fahrweg_id,
+    fsi.fw_no,
+    fsi.fw_lang
+from
+    vbzdat.fahrzeiten_soll_ist fsi
+   where fsi.linie =4;
+  */
+   
+
+ /* 8b View erstellen   */
+
+   CREATE OR REPLACE VIEW vbzdat.query_line
+AS select
+    fsi.linie,
+    fsi.richtung,
+    fsi.fahrweg_id,
+    fsi.fw_no,
+    fsi.fw_lang
+from
+    vbzdat.fahrzeiten_soll_ist fsi
+    where fsi.linie =4;
+
+select * from   vbzdat.fahrzeiten_soll_ist fsi;
+
---
+++ Scripts/Aufgabe_8c.sql
@@ -0,0 +1,14 @@
+use vbzdat;
+
+CREATE TABLE linie (PRIMARY KEY (fahrweg_id))
+SELECT DISTINCT
+	fsi.fahrweg_id, 
+    fsi.linie,
+    fsi.richtung,
+    fsi.fw_no,
+    fsi.fw_lang
+from
+    vbzdat.fahrzeiten_soll_ist fsi
+   where fsi.linie =4;
+
+ select *from vbzdat.linie;  /* zeigt die erstellte Tabelle an. */ 
